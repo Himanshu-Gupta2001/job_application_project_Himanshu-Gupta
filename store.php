f3545b73762e396d56af976d1362a8f7fa7658af
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2021-07-28 11:02:15 +0530

    commit

--- store.php
+++ store.php
@@ -2,11 +2,26 @@
 $name=$_GET['t1'];
 $pass=$_GET['t2'];
 $email=$_GET['t3'];
+$language=$_GET['t4'];
 
-$file = fopen("details.txt", 'a');
-$text = $name."::".$pass."::".$email."::"."\r\n";
+$file = fopen("details.txt", 'a+');
+$s=fread($file, filesize("details.txt"));
+$pos = strpos($s, $email);
+if(!$pos){
+$f=fopen("jobAppNumber.txt", "r");
+$j_a_n=fread($f, filesize("jobAppNumber.txt"));
+fclose($f);
+$f=fopen("jobAppNumber.txt", "w");
+$text=$name."::".$pass."::".$email."::".$language."::".$j_a_n."::\r\n";
+$j_a_n = $j_a_n + 1;
+fwrite($f,$j_a_n);
 fwrite($file, $text);
 fclose($file);
-echo "Your data submitted successfully<br>";
+fclose($f);
+echo "Your data has been submitted successfully<br>";
+}
+else {
+echo "You are already registered";
+}
 echo "<a href=login.html>Click Here to login</a>";
 ?>
--- validate.php
+++ validate.php
@@ -7,14 +7,42 @@ $file = fopen("details.txt", 'r');
 while(!feof($file))
   {
   $s=fgets($file);
-  $str_arr = array_pad(explode('::', $s),3,null);
+  $str_arr = array_pad(explode('::', $s),5,null);
   if ($str_arr[1]==$psw and $str_arr[2]==$email){
-		echo "Authorized User";
-		$flag=1;
-	  	break;
-	}//end if 
+		echo "Congratulations! You are authorized user"."<br>";
+    echo "Your Job Application number is ".$str_arr[4]."<br>";
+?>
+
+<form action="answer.txt">
+<table border=1>
+<tr>
+<td>Choose Your Programming language
+<td colspan=10>
+<select name="lang">
+<option value=".java">Java</option>
+<option value=".py">Python</option>
+</select>
+</tr>
+<tr>
+<td colspan=11 align=center><textarea name="program" rows=20 cols=150></textarea>
+</tr>
+<tr>
+<td colspan=11 align=center><input type=submit value="SUBMIT">
+</tr>
+</table>
+  </form>
+
+<?php
+
+      $flag=1;
+      break;
+    }//end if 
   }//end while
 if ($flag==0)
-	echo "Please register before login";
-fclose($file);
+echo "Please register yourself before login";
+  fclose($file);
 ?>
+
+
+
+
