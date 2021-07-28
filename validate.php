a5034df8b7d6619916578e9dff98e90d75bc47a5
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2021-07-28 14:06:23 +0530

    Final commit

--- validate.php
+++ validate.php
@@ -25,6 +25,114 @@ while(!feof($file))
       echo "$str";
       fclose($solve);
     }
+    else if($ques == 3){
+      $solve=fopen("ques3.txt", 'r');
+      $str = fread($solve, filesize("ques3.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 4){
+      $solve=fopen("ques4.txt", 'r');
+      $str = fread($solve, filesize("ques4.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 5){
+      $solve=fopen("ques5.txt", 'r');
+      $str = fread($solve, filesize("ques5.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 6){
+      $solve=fopen("ques6.txt", 'r');
+      $str = fread($solve, filesize("ques6.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 7){
+      $solve=fopen("ques7.txt", 'r');
+      $str = fread($solve, filesize("ques7.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 8){
+      $solve=fopen("ques8.txt", 'r');
+      $str = fread($solve, filesize("ques8.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 9){
+      $solve=fopen("ques9.txt", 'r');
+      $str = fread($solve, filesize("ques9.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 10){
+      $solve=fopen("ques10.txt", 'r');
+      $str = fread($solve, filesize("ques10.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 11){
+      $solve=fopen("ques11.txt", 'r');
+      $str = fread($solve, filesize("ques11.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 12){
+      $solve=fopen("ques12.txt", 'r');
+      $str = fread($solve, filesize("ques12.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 13){
+      $solve=fopen("ques13.txt", 'r');
+      $str = fread($solve, filesize("ques13.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 14){
+      $solve=fopen("ques14.txt", 'r');
+      $str = fread($solve, filesize("ques14.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 15){
+      $solve=fopen("ques15.txt", 'r');
+      $str = fread($solve, filesize("ques15.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 16){
+      $solve=fopen("ques16.txt", 'r');
+      $str = fread($solve, filesize("ques16.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 17){
+      $solve=fopen("ques17.txt", 'r');
+      $str = fread($solve, filesize("ques17.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 18){
+      $solve=fopen("ques18.txt", 'r');
+      $str = fread($solve, filesize("ques18.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else if($ques == 19){
+      $solve=fopen("ques19.txt", 'r');
+      $str = fread($solve, filesize("ques19.txt"));
+      echo "$str";
+      fclose($solve);
+    }
+    else {
+      $solve=fopen("ques20.txt", 'r');
+      $str = fread($solve, filesize("ques20.txt"));
+      echo "$str";
+      fclose($solve);
+    }
 ?>
 
 <form action="answer.txt">
